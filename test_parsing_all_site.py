871ab7d5727b19e55fa19cf359b054ae3c446c58
Anton-khv-27 <anto_xa@inbox.ru>
2025-08-05 15:07:50 +0300

    test_parsing_all_site.py

--- test_parsing_all_site.py
+++ test_parsing_all_site.py
@@ -16,10 +16,13 @@ driver.maximize_window()
 load_dotenv()
 
 # Настройка Selenium
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=options)
 
 driver = webdriver.Chrome(options=chrome_options)
 start_time = time()
@@ -141,5 +144,6 @@ if total_errors > 0:
 
 # Не закрываем сразу, чтобы увидеть результат
 input("Нажмите Enter для выхода...")
+
 
 driver.quit()
