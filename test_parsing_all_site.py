b31ee803465a9fef3e5d7c3a829b670ac8a83739
Anton-khv-27 <anto_xa@inbox.ru>
2025-08-05 15:11:14 +0300

    test_parsing_all_site.py

--- test_parsing_all_site.py
+++ test_parsing_all_site.py
@@ -21,6 +21,7 @@ options.add_argument('--headless')
 options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--user-data-dir=/tmp/chrome-data')  # уникальная временная директория
 
 driver = webdriver.Chrome(options=options)
 
@@ -147,3 +148,4 @@ input("Нажмите Enter для выхода...")
 
 
 driver.quit()
+
