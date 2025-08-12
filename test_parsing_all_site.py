f11b5ca4eacafa8f02f2b3401cf1550af70be906
Anton-khv-27 <anto_xa@inbox.ru>
2025-08-12 21:35:08 +0300

    test_parsing_all_site.py

--- test_parsing_all_site.py
+++ test_parsing_all_site.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.support import expected_conditions as EC
 import os
@@ -155,5 +156,6 @@ if total_errors > 0:
 #input("Нажмите Enter для выхода...")
 
 driver.quit()
+
 
 
